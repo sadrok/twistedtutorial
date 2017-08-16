79babee6c90ed61cbd739ff723cfcc49b5f40df6
Francois Basson <sadrok.za@gmail.com>
2017-08-16 16:10:44 +0200

    Play with routes with regex and stuff

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -8,5 +8,8 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+    config.add_route('home-with-words', '/{words:[a-zA-Z ]+}')
+    config.add_route('home-with-digits', '/{digits:[\d+.,]+}')
+    config.add_route('home-with-uuid', '/{uuid:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}}')
     config.scan()
     return config.make_wsgi_app()
--- myproject/views/root.py
+++ myproject/views/root.py
@@ -2,5 +2,9 @@ from pyramid.view import view_config
 
 
 @view_config(route_name='home', renderer='myproject:templates/mytemplate.jinja2')
+@view_config(route_name='home-with-words', renderer='myproject:templates/mytemplate.jinja2')
+@view_config(route_name='home-with-digits', renderer='myproject:templates/mytemplate.jinja2')
+@view_config(route_name='home-with-uuid', renderer='myproject:templates/mytemplate.jinja2')
 def index(request):
     return {'project': 'myproject'}
+
